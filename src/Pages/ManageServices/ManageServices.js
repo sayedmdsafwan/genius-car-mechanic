02d2161201f19c059e0a67e8cb30da985b330eb6
Md. Safwan <sayedmdsafwan@gmail.com>
2022-04-22 16:34:24 +0600

    finish designing

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -2,12 +2,21 @@ import React from "react";
 import useServices from "../../hooks/useServices";
 
 const ManageServices = () => {
-    const [services] = useServices();
+    const [services, setServices] = useServices();
 
     const handleDelete = (id) => {
         const proceed = window.confirm("Are you sure?");
         if (proceed) {
-            console.log("clicked", id);
+            const url = `http://localhost:4000/service/${id}`;
+            fetch(url, {
+                method: "DELETE",
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    const remaining = services.filter((s) => s._id !== id);
+                    setServices(remaining);
+                });
         }
     };
 
