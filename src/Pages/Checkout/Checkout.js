f8896de9e08a2a773f035bebdb2cec1d9ca4bcdc
Md. Safwan <sayedmdsafwan@gmail.com>
2022-04-26 16:32:39 +0600

    complete user info

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,16 +1,74 @@
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useParams } from "react-router-dom";
+import auth from "../../firebase.init";
 import useServiceDetail from "../../hooks/useServiceDetail";
 
 const Checkout = () => {
     const { serviceId } = useParams();
-
+    const [user] = useAuthState(auth);
     const [service] = useServiceDetail(serviceId);
+
+    const handlePlaceOrder = (event) => {
+        event.preventDefault();
+        const order = {
+            email: user.email,
+            service: service.name,
+            serviceId: serviceId,
+            address: event.target.address.value,
+            phone: event.target.phone.value,
+        };
+    };
 
     return (
         <div className="w-50 mx-auto">
             <h1>Please Checkout {service.name}</h1>
-            <form>
-                <input type="text" placeholder="Name" name="name" required />
+            <form onSubmit={handlePlaceOrder}>
+                <input
+                    className="w-100 mb-2 "
+                    type="text"
+                    value={user.displayName}
+                    placeholder="Name"
+                    name="name"
+                    required
+                    readOnly
+                />
+                <br />
+                <input
+                    className="w-100 mb-2 "
+                    type="email"
+                    value={user.email}
+                    placeholder="Email"
+                    name="email"
+                    required
+                    readOnly
+                    disabled
+                />
+                <br />
+                <input
+                    className="w-100 mb-2 "
+                    type="text"
+                    placeholder="Service"
+                    name="service"
+                    required
+                />
+                <br />
+                <input
+                    className="w-100 mb-2 "
+                    type="text"
+                    placeholder="Address"
+                    name="address"
+                    required
+                />
+                <br />
+                <input
+                    className="w-100 mb-2 "
+                    type="text"
+                    placeholder="Phone"
+                    name="phone"
+                    required
+                />
+                <br />
+                <input type="submit" value="Place Order" />
             </form>
         </div>
     );
